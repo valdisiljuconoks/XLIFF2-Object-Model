f6eeb50c22c3e77a96c193d577cf58a34203435d
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2019-03-16 07:02:15 +0200

    signed the package

--- Xliff.OM/Properties/AssemblyInfo.cs
+++ Xliff.OM/Properties/AssemblyInfo.cs
@@ -23,7 +23,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
 
-[assembly: InternalsVisibleTo("Xliff.OM.Tests")]
+[assembly: InternalsVisibleTo("Xliff.OM.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010061f7a9aa02c1a525f7e914f39ac8901fc05fbdbad295b3c3b17e168fb3e70818c453da4f6ccec386f92038352e2b040a393ea85e631a3da420d92ca1b39cd346a9d2bbe8ef5374d7eec997c7a2a2a93e7ce45a554efe561cadb6f10b86072b79d732729a8f3d43756e6f52c28543ed2ab2822e3dcc99ec25fe5f17bb02976fc0")]
